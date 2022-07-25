641afc2c0244f9f295af5321a36b26f5392f6d21
arianiapalmer <arianiapalmer@gmail.com>
2022-07-25 01:52:53 -0400

    put beasts into cards

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,27 +1,33 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+
 
 class HornedBeast extends React.Component {
-    constructor (props){
+    constructor(props) {
         super(props);
         this.state = {
             favorites: 0
         }
     }
-    voteClick = () => this.setState({favorites: this.state.favorites + 1});
+    voteClick = () => this.setState({ favorites: this.state.favorites + 1 });
     render() {
         return (
-            <div className="beast">
-                <h2>{this.props.title}</h2>
-                <p>{this.props.description}</p>
-                <p>👹: {this.state.favorites}</p>
-                <img
+            <Card style={{ width: '18rem' }} className="beast">
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Card.Text>👹: {this.state.favorites}</Card.Text>
+                </Card.Body>
+                <Card.Img
                     src={this.props.img}
                     alt={this.props.description}
                     onClick={this.voteClick}
                 />
-            </div>
+            </Card>
         )
     }
 }
 
 export default HornedBeast;
+
--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
 import beasts from './data.json';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 
 class Main extends React.Component {
     render() {
         return (
-            <>
-                {beasts.map((index,i) =>
-                    <HornedBeast
-                        title={beasts[i].title}
-                        img={beasts[i].image_url}
-                        description={beasts[i].description}
-                    />
-                )}
-            </>
+            <Container>
+                <Row xs={1} sm={2} md={3} lg={4} className="g-4">
+                    {beasts.map((index, i) =>
+                        <Col>
+                            <HornedBeast
+                                title={beasts[i].title}
+                                img={beasts[i].image_url}
+                                description={beasts[i].description}
+                            />
+                        </Col>
+                    )}
+                </Row>
+            </Container>
 
         )
     }
 }
 
 export default Main;
+
