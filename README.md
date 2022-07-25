97fe51de15bc27b7e90fefc843420e6b9b2d00d8
arianiapalmer <arianiapalmer@gmail.com>
2022-07-25 01:00:59 -0400

    feature 2 completed

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ Start time: _____
 Finish time: _____
 
 Actual time needed to complete: _____
+_____________________________________________________________________________________
+Number and name of feature: Feature #2: Allow users to vote for their favorite beast
+
+Estimate of time needed to complete: 1hr
+
+Start time: 11:11pm
+
+Finish time: _____
+
+Actual time needed to complete: _____
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
-    // constructor (props){
-    //     super(props);
-    //     this.state = {
-
-    //     }
-    // }
+    constructor (props){
+        super(props);
+        this.state = {
+            favorites: 0
+        }
+    }
+    voteClick = () => this.setState({favorites: this.state.favorites + 1});
     render() {
         return (
             <div className="beast">
                 <h2>{this.props.title}</h2>
                 <p>{this.props.description}</p>
+                <p>👹: {this.state.favorites}</p>
                 <img
                     src={this.props.img}
                     alt={this.props.description}
+                    onClick={this.voteClick}
                 />
             </div>
         )
